0cd217f5adb5b8c93cc132bfe315ec8a25aede6e
Michal Burzynski (PGS Software) <mburzynski@pgs-soft.com>
2022-04-27 02:27:35 +0200

    Import routing modules

--- src/app/routes/approval/approval.module.ts
+++ src/app/routes/approval/approval.module.ts
@@ -1,9 +1,10 @@
 import { ApprovalComponent } from './approval.component';
+import { ApprovalRoutingModule } from './approval-routing.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ApprovalRoutingModule],
   declarations: [ApprovalComponent],
 })
 export class ApprovalModule {}
--- src/app/routes/timesheets/timesheets.module.ts
+++ src/app/routes/timesheets/timesheets.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { TimesheetAddComponent } from './components/timesheet-add/timesheet-add.component';
 import { TimesheetListComponent } from './components/timesheet-list/timesheet-list.component';
+import { TimesheetsRoutingModule } from './timesheets-routing.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, TimesheetsRoutingModule],
   declarations: [TimesheetListComponent, TimesheetAddComponent],
 })
 export class TimesheetsModule {}
